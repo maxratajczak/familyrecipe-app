22b60e2f5c5800ecbd3fc5c821603e8d00411612
Max Ratajczak <75450072+maxratajczak@users.noreply.github.com>
2021-10-19 05:00:40 -0400

    Update cmdlinecolor.js

--- js/cmdlinecolor.js
+++ js/cmdlinecolor.js
@@ -2,9 +2,9 @@ const clc = require("cli-color");
 
 module.exports = {
 
-    error: clc.xterm(196),
-    warn: clc.xterm(208),
-    success: clc.greenBright, // xterm(118)
-    notice: clc.xterm(39)
+    error: clc.redBright.bold, // xterm(196)
+    warn: clc.yellowBright.bold, // xterm(208)
+    success: clc.greenBright.bold, // xterm(118)
+    notice: clc.cyanBright.bold // xterm(39)
 
 }

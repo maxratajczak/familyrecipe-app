470f78e204e9d8ea131f708ca6f86f9a23095ca4
Max Ratajczak <75450072+maxratajczak@users.noreply.github.com>
2021-11-09 15:14:11 -0500

    Fixed Safari Bugs

--- js/navbar-dropdown.js
+++ js/navbar-dropdown.js
@@ -1,1 +1,10 @@
+var recipesBttn = document.querySelector("#recipe-dropdown");
+var dropdownMenu = document.querySelector("#landing-dropdown ul");
 
+recipesBttn.addEventListener("mouseclick", () => {
+    dropdownMenu.style.opacity = "100%";
+});
+
+function focus() {
+    dropdownMenu.style.opacity = "100%";
+}

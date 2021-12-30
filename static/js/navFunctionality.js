13f2e0802cd13465407c611233c05c99345c80c9
Max Ratajczak <75450072+maxratajczak@users.noreply.github.com>
2021-12-29 20:53:09 -0500

    Fixed Mobile Nav & Appetizer Page

---
+++ static/js/navFunctionality.js
@@ -0,0 +1,62 @@
+$(function() {
+	FastClick.attach(document.body);
+});
+
+// Mobile Menu
+$("#hamburger-button").click(() => {
+    $("#mobile-navbar-links").toggle()
+    //$("#mobile-navbar-links").toggle().addClass("animate__animated animate__fadeIn");
+    if ($("#mobile-nav").hasClass("nav-blurred")) {
+        removeClasses();
+    }
+    else {
+        addClasses();
+    }
+})
+
+// Desktop Menu
+$("#recipe-dropdown").on("click", () => {
+    $("#dropdown-links").toggle();
+})
+
+// Outside Clicking
+$('html').on('click', function(e) {
+    if($(e.target).closest('#landing-dropdown').length === 0) {
+        $('#dropdown-links').hide();
+    }
+});
+
+$('html').on('click', function(e) {
+    if($(e.target).closest('#mobile-nav').length === 0) {
+        $('#mobile-navbar-links').hide();
+        removeClasses();
+    }
+});
+
+window.onresize = () => {
+    if(window.innerWidth > 900) {
+        $('#mobile-navbar-links').hide();
+        removeClasses();
+    }
+    if($("body").hasClass("noScroll") && $("body").hasClass("noScroll")) {
+    }
+}
+
+
+function addClasses() {
+    $("#mobile-nav").addClass("nav-blurred")
+    // $("body").addClass("noScroll")
+    // $("html").addClass("noScroll")
+    // $(".bodyUnfocused").addClass("bodyDisabled")
+    $("#hamburger-button i").removeClass("fa-bars");
+    $("#hamburger-button i").addClass("fa-times");
+}
+
+function removeClasses() {
+    $("#mobile-nav").removeClass("nav-blurred")
+    // $("body").removeClass("noScroll")
+    // $("html").removeClass("noScroll")
+    // $(".bodyUnfocused").removeClass("bodyDisabled")
+    $("#hamburger-button i").addClass("fa-bars");
+    $("#hamburger-button i").removeClass("fa-times");
+}

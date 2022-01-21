1ea22bf88769881727dc869db495addaab937aa1
Max Ratajczak <75450072+maxratajczak@users.noreply.github.com>
2022-01-20 19:42:09 -0500

    Head helper test

--- app.js
+++ app.js
@@ -56,6 +56,11 @@ app.engine(".hbs", exphbs.engine({
             else {
                 return options.fn(this);
             }
+        },
+        section: function(name, options) {
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
         }
     }
 }))
